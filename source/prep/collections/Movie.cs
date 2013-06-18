798c73753ca7b47d0e79db69d463b75ba8a10c2c
Raghavendra Prasad K <rprasadk@gmail.com>
2013-06-18 13:54:15 +1000

    Pushing new changes

--- source/prep/collections/Movie.cs
+++ source/prep/collections/Movie.cs
@@ -2,12 +2,26 @@ using System;
 
 namespace prep.collections
 {
-  public class Movie
+  public class Movie : IEquatable<Movie>
   {
     public string title { get; set; }
     public ProductionStudio production_studio { get; set; }
     public Genre genre { get; set; }
     public int rating { get; set; }
     public DateTime date_published { get; set; }
+
+    public bool Equals(Movie other)
+    {
+        if (ReferenceEquals(this, other)) return true;
+
+        return (this.title.Equals(other.title));
+    }
+
+      public bool Satisfies(Predicate<Movie> func)
+      {
+          return func(this);
+      }
+
+
   }
 }
--- source/prep/collections/MovieLibrary.cs
+++ source/prep/collections/MovieLibrary.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace prep.collections
 {
@@ -14,52 +15,74 @@ namespace prep.collections
 
     public IEnumerable<Movie> all_movies()
     {
-      return this.movies;
+        foreach (var movie in movies)
+        {
+            yield return movie;
+        }
     }
 
     public void add(Movie movie)
     {
-      throw new NotImplementedException();
+        if (!movies.Contains(movie))
+        {
+            movies.Add(movie);
+        }
     }
 
     public IEnumerable<Movie> all_movies_published_by_pixar()
     {
-      throw new NotImplementedException();
+        return GetSatisfyingMovies(m => m.production_studio == ProductionStudio.Pixar);
     }
-
+     
     public IEnumerable<Movie> all_movies_published_by_pixar_or_disney()
     {
-      throw new NotImplementedException();
+        return GetSatisfyingMovies(m => m.production_studio == ProductionStudio.Pixar || m.production_studio == ProductionStudio.Disney);
     }
 
     public IEnumerable<Movie> all_movies_not_published_by_pixar()
-    {
-      throw new NotImplementedException();
+    {       
+        return GetSatisfyingMovies(m => m.production_studio != ProductionStudio.Pixar);
     }
 
     public IEnumerable<Movie> all_movies_published_after(int year)
-    {
-      throw new NotImplementedException();
+    {       
+        return GetSatisfyingMovies(m => m.date_published.Year > year);
     }
 
     public IEnumerable<Movie> all_movies_published_between_years(int startingYear, int endingYear)
-    {
-      throw new NotImplementedException();
+    {        
+        return GetSatisfyingMovies(m => m.date_published.Year >= startingYear && m.date_published.Year <= endingYear);
     }
 
     public IEnumerable<Movie> all_kid_movies()
     {
-      throw new NotImplementedException();
+        return GetSatisfyingMovies(m => m.genre == Genre.kids);
     }
 
     public IEnumerable<Movie> all_action_movies()
     {
-      throw new NotImplementedException();
+        return GetSatisfyingMovies(m => m.genre == Genre.action);
+    }
+
+    private IEnumerable<Movie> GetSatisfyingMovies(Predicate<Movie> condition)
+    {
+        foreach (var movie in movies)
+        {
+            if (movie.Satisfies(condition))
+            {
+                yield return movie;
+            }
+        }
     }
 
     public IEnumerable<Movie> sort_all_movies_by_title_descending()
     {
-      throw new NotImplementedException();
+      var sortedList = new List<Movie>(movies);
+      sortedList.Sort(new MovieComparer((m1, m2) => m2.title.CompareTo(m1.title)));
+        foreach (var movie in sortedList)
+        {
+            yield return movie;
+        }      
     }
 
     public IEnumerable<Movie> sort_all_movies_by_title_ascending()
@@ -82,4 +105,19 @@ namespace prep.collections
       throw new NotImplementedException();
     }
   }
+
+    public class MovieComparer : IComparer<Movie>
+    {
+        private readonly Func<Movie, Movie, int> _comp;
+
+        public MovieComparer(Func<Movie, Movie, int> comp)
+        {
+            _comp = comp;
+        }
+
+        public int Compare(Movie x, Movie y)
+        {
+            return _comp(x, y);
+        }
+    }
 }

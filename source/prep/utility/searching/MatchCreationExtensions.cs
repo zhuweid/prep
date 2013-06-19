7d06f934c4fbf107577770a55edcbd4738d3489b
zhuweid <zhuweid03@gmail.com>
2013-06-19 13:46:10 +1000

    Pushing new changes

--- source/prep/utility/searching/MatchCreationExtensions.cs
+++ source/prep/utility/searching/MatchCreationExtensions.cs
@@ -29,10 +29,10 @@ namespace prep.utility.searching
       return equal_to(extension_point, value).not();
     }
 
-    public static IMatchA<Target> greater_than<Target,PropertyType>(this MatchCreationExtensionPoint<Target,PropertyType> extension_point, PropertyType comparison_value) 
+    public static IMatchA<Target> greater_than<Target, PropertyType>(this MatchCreationExtensionPoint<Target, PropertyType> extension_point, PropertyType comparison_value)
      where PropertyType : IComparable<PropertyType>
     {
-      return create_from_attribute_criteria(extension_point, new IsGreaterThan<PropertyType>(comparison_value));
+        return create_from_attribute_criteria(extension_point, new IsGreaterThan<PropertyType>(comparison_value));
     }
 
     public static IMatchA<Target> between<Target,PropertyType>(this MatchCreationExtensionPoint<Target,PropertyType> extension_point, PropertyType start, PropertyType end) 
@@ -41,4 +41,13 @@ namespace prep.utility.searching
       return create_from_attribute_criteria(extension_point, new IsBetween<PropertyType>(start, end));
     }
   }
+
+    public static class MatchCreationSepcificExtensions
+    {
+        public static IMatchA<Target> greater_than<Target>(this MatchCreationExtensionPoint<Target, DateTime> extension_point, int comparison_value)
+        {
+            return extension_point.create_from_attribute_criteria(new IsGreaterThan<DateTime>(new DateTime(comparison_value, 12, 31)));
+        }
+
+    }
 }

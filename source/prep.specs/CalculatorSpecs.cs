723ccfee347ecfcdc66dbdfd56f9134e6a9744ce
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2013-06-20 00:12:12 -0600

    Updated

--- source/prep.specs/CalculatorSpecs.cs
+++ source/prep.specs/CalculatorSpecs.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
+using System.Security;
+using System.Security.Principal;
+using System.Threading;
+using Machine.Fakes;
 using Machine.Specifications;
+using Rhino.Mocks;
 using developwithpassion.specifications.extensions;
 using developwithpassion.specifications.rhinomocks;
 using prep.calculator;
@@ -21,6 +27,7 @@ namespace prep.specs
         command = fake.an<IDbCommand>();
 
         connection.setup(x => x.CreateCommand()).Return(command);
+
       };
 
       Because b = () =>
@@ -38,6 +45,28 @@ namespace prep.specs
       static int result;
       static IDbConnection connection;
       static IDbCommand command;
+    }
+
+    public class when_shutting_off_the_calculator : concern
+    {
+      public class and_they_are_in_the_incorrect_security_group
+      {
+        Establish c = () =>
+        {
+          principal = fake.an<IPrincipal>();
+          principal.setup(x => x.IsInRole(Arg<String>.Is.Anything)).Return(false);
+
+          spec.change(() => Thread.CurrentPrincipal).to(principal);
+        };
+
+        Because b = () =>
+          spec.catch_exception(() => sut.shut_off());
+
+        It should_throw_a_security_exception = () =>
+          spec.exception_thrown.ShouldBeAn<SecurityException>();
+
+        static IPrincipal principal;
+      }
     }
 
     public class when_attempting_to_add_a_negative_to_a_positive : concern
--- source/prep/calculator/Calculator.cs
+++ source/prep/calculator/Calculator.cs
@@ -3,25 +3,33 @@ using System.Data;
 
 namespace prep.calculator
 {
-    public class Calculator
+  public class Calculator
+  {
+    IDbConnection connection;
+
+    public Calculator(IDbConnection connection, int offset, int offset2)
     {
-        IDbConnection connection;
-
-        public Calculator(IDbConnection connection)
-        {
-            this.connection = connection;
-        }
-
-        public int add(int i, int i1)
-        {
-            if (i * i1 < 0)
-                throw new ArgumentException();
-
-            connection.Open();
-            var command = connection.CreateCommand();
-            command.ExecuteNonQuery();
-
-            return i + i1;
-        }
+      this.connection = connection;
     }
+
+    public int add(int i, int i1)
+    {
+      if (i*i1 < 0)
+        throw new ArgumentException();
+
+      using(connection)
+      using (var command = connection.CreateCommand())
+      {
+        connection.Open();
+        command.ExecuteNonQuery();
+      }
+
+      return i + i1;
+    }
+
+    public void shut_off()
+    {
+      throw new NotImplementedException();
+    }
+  }
 }

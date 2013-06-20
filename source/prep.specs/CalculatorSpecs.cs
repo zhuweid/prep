0d5c7c6565160e71dd2ac8568c14831a353e00de
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2013-06-19 23:25:42 -0600

    Updated

--- source/prep.specs/CalculatorSpecs.cs
+++ source/prep.specs/CalculatorSpecs.cs
@@ -1,19 +1,36 @@
-﻿using Machine.Specifications;
+﻿using System;
+using Machine.Specifications;
+using developwithpassion.specifications.rhinomocks;
 using prep.calculator;
+using developwithpassion.specifications.extensions;
 
 namespace prep.specs
 {
   public class CalculatorSpecs
   {
-    public class when_adding_two_numbers
+    public abstract class concern : Observes<Calculator>
+    {
+    }
+
+    public class when_adding_two_numbers : concern
     {
       Because b = () =>
-        result = Calculator.add(2, 3);
+        result = sut.add(2, 3);
 
       It should_return_the_sum = () =>
         result.ShouldEqual(5);
 
       static int result;
+    }
+
+    public class when_attempting_to_add_a_negative_to_a_positive : concern
+    {
+      Because b = () =>
+        spec.catch_exception(() => sut.add(-2, 3));
+
+      It should_throw_an_argument_exception = () =>
+        spec.exception_thrown.ShouldBeAn<ArgumentException>();
+
     }
   }
 }
--- source/prep/calculator/Calculator.cs
+++ source/prep/calculator/Calculator.cs
@@ -2,7 +2,7 @@ namespace prep.calculator
 {
     public class Calculator
     {
-        public static int add(int i, int i1)
+        public int add(int i, int i1)
         {
             return i + i1;
         }

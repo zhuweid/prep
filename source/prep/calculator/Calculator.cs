0a1101fe760d794c949fd5419a6565b64aa73421
zhuweid <zhuweid03@gmail.com>
2013-06-20 15:45:11 +1000

    Pushing new changes

--- source/prep/calculator/Calculator.cs
+++ source/prep/calculator/Calculator.cs
@@ -1,13 +1,24 @@
 using System;
+using System.Data;
 
 namespace prep.calculator
 {
     public class Calculator
     {
+        private readonly IDbConnection dbConnection;
+
+
+        public Calculator(IDbConnection dbConnection)
+        {
+            this.dbConnection = dbConnection;
+        }
+
         public int add(int i, int i1)
         {
             if (i * i1 < 0)
                 throw new ArgumentException();
+
+            this.dbConnection.Open();
 
             return i + i1;
         }

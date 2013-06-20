192b23852c5bf2cae0ce7c4c0c43074477fba15f
zhuweid <zhuweid03@gmail.com>
2013-06-20 15:29:04 +1000

    Pushing new changes

--- source/prep/calculator/Calculator.cs
+++ source/prep/calculator/Calculator.cs
@@ -6,7 +6,7 @@ namespace prep.calculator
     {
         public int add(int i, int i1)
         {
-            if (i < 0 || i1 < 0)
+            if (i * i1 < 0)
                 throw new ArgumentException();
 
             return i + i1;
